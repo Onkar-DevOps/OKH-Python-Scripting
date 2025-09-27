d8f605127386a0b98cb514a764226ff9ae22c23d
Onkar <onkarkhandale36@gmail.com>
2025-09-27 12:09:04 +0530

    Variables file

---
+++ Basics/Variables.py
@@ -0,0 +1,14 @@
+# Storing value in var and printing it.
+name = input("What is your name?")
+print(name)
+
+#Using multi var
+name = "Onkar"
+print(name)
+
+name = "Khandale"
+print(name)
+
+#Length function
+name = input("What is your name?")
+print(len(name))
